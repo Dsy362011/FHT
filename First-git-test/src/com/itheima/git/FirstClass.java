8154579e8b6b3357d95704338de11db95efb9824
dsy <362011653@qq.com>
2020-03-07 20:35:03 +0800

    添加到git

---
+++ First-git-test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @description:
+ * @author: dingsy
+ * @createDate: 2020/3/7
+ * @version: 1.0
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
